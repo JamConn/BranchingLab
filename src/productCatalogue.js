a7ef57a7693fedf9c997130fffeca7acde76be39
Jamie Conlon <20085217@mail.wit.ie>
2023-11-21 18:00:49 +0000

    Tested and implemented Search function

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -51,4 +51,14 @@ export class Catalogue {
       .reduce((acc, p) => acc + 1, 0);
     return noProductsAdded;
   }
+
+  search(criteria) {
+    if (criteria.keyword == undefined && criteria.price == undefined) {
+      throw new Error("Bad search");
+    }
+    const nameSearch = (product) => product.name.search(criteria.keyword) >= 0;
+    const priceSearch = (product) => product.price <= criteria.price;
+    const searchFunction = criteria.keyword ? nameSearch : priceSearch;
+    return this.products.filter(searchFunction);
+  }
 }
